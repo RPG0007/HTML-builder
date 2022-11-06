04616531fd0a0c74cfc6f11b3002d9969694d5b5
RPG0007 <90514460+RPG0007@users.noreply.github.com>
2022-11-06 22:06:34 +0300

    feat:task4 copydir done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
 
+const fs = require('fs');
+const path = require('node:path');
+const newDirectoryPath = path.join(__dirname, 'files-copy');
+const directoryPath = path.join(__dirname, 'files');
+
+fs.mkdir(newDirectoryPath, { recursive: true }, (err) => {
+
+  if (err) {
+    console.error(err)
+    return;
+  }
+
+})
+
+fs.readdir(directoryPath, (err, files) => {
+
+  if (err) {
+    console.error(err)
+    return;
+  }
+
+  files.forEach(element => {
+    fs.copyFile(`${directoryPath}/${element}`, `${newDirectoryPath}/${element}`, err => {
+
+      if(err) throw err;  
+
+    });
+
+  });
+})
