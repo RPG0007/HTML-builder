d04eb71334e15253874ffc81c05b4382990d5bd7
RPG0007 <90514460+RPG0007@users.noreply.github.com>
2022-11-09 23:46:24 +0300

    fix:fix task 4 updating

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 const path = require('node:path');
 const newDirectoryPath = path.join(__dirname, 'files-copy');
 const directoryPath = path.join(__dirname, 'files');
+fs.readdir(newDirectoryPath, function (err, files) {
+  files?.forEach(file => fs.unlink(`./04-copy-directory/files-copy/${file}`, ()=>{}))
+});
 fs.mkdir(newDirectoryPath, { recursive: true }, (err) => {
   if (err) {
     console.error(err)
